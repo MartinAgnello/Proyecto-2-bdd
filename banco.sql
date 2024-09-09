f1d176f80c86f1f9fdc04140a4692d366e225cf0
MaxiFernandez112002 <maxicapox3@outlook.com>
2024-09-09 18:14:18 -0300

    V B

--- banco.sql
+++ banco.sql
@@ -325,6 +325,61 @@ CREATE TABLE transferencia (
 ) ENGINE=InnoDB;
 
 
+#-------------------------------------------------------------------------
+# Creación de usuarios y otorgamiento de privilegios
+
+
+    CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
+    GRANT ALL PRIVILEGES ON banco.* TO 'admin'@'localhost' WITH GRANT OPTION;
+
+
+    CREATE USER 'empleado'@'%' IDENTIFIED BY 'empleado';
+
+    -- Privilegios sólo de consulta (SELECT) sobre las tablas Empleado, Sucursal, Tasa Plazo Fijo y Tasa Prestamo
+    GRANT SELECT ON banco.empleado TO 'empleado'@'%';
+    GRANT SELECT ON banco.sucursal TO 'empleado'@'%';
+    GRANT SELECT ON banco.tasa_plazo_fijo TO 'empleado'@'%';
+    GRANT SELECT ON banco.tasa_prestamo TO 'empleado'@'%';
+
+    -- Privilegios de consulta (SELECT) e inserción (INSERT) sobre las tablas Préstamo, Plazo Fijo, Plazo Cliente, Caja Ahorro y Tarjeta
+    GRANT SELECT, INSERT ON banco.prestamo TO 'empleado'@'%';
+    GRANT SELECT, INSERT ON banco.plazo_fijo TO 'empleado'@'%';
+    GRANT SELECT, INSERT ON banco.plazo_cliente TO 'empleado'@'%';
+    GRANT SELECT, INSERT ON banco.caja_ahorro TO 'empleado'@'%';
+    GRANT SELECT, INSERT ON banco.tarjeta TO 'empleado'@'%';
+
+    -- Privilegios de consulta (SELECT), inserción (INSERT) y modificación (UPDATE) sobre las tablas Cliente CA, Cliente y Pago
+    GRANT SELECT, INSERT, UPDATE ON banco.cliente_ca TO 'empleado'@'%';
+    GRANT SELECT, INSERT, UPDATE ON banco.cliente TO 'empleado'@'%';
+    GRANT SELECT, INSERT, UPDATE ON banco.pago TO 'empleado'@'%';
+
+
+    -- Eliminamos el usuario vacio
+    DROP USER ''@'localhost';
+
+
+    CREATE USER 'atm'@'%' IDENTIFIED BY 'atm';
+
+    GRANT SELECT ON banco.caja_ahorro TO 'atm'@'%';
+    GRANT INSERT banco.transaccion TO 'atm'@'%'; /*PREGUNTAR*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
